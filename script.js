1bb59a9016e297befbb5dfdcad09dccb8e757dc4
ShafiulTuhin <tuhin6488@gmail.com>
2026-02-11 22:29:33 +0600

    added some classes

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 console.log((document.querySelector(".message").innerHTML = "Correct Number!"));
+document.querySelector(".number").textContent = 15;
+document.querySelector(".highscore").textContent = 20;
